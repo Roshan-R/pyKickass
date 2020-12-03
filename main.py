ec9af023fefb865a84ac5381b0b8fd35352eef54
Roshan-R <roshanr2001@gmail.com>
2020-12-03 11:35:37 +0530

    added AIM support, changed template

--- main.py
+++ main.py
@@ -13,13 +13,23 @@ class kickass:
         self.kick()
         self.save_text()
 
-
     def save_text(self):
-        with open("output.tex", "w+") as output:
+        with open("outputfolder/output.txt", "w") as output:
+            print(type(self.text))
             output.write(self.text)
             print("Output is written as output.tex")
 
+        os.rename("outputfolder/output.txt", "outputfolder/output.tex")
+        self.zipPref = input("Do you want to make a .zip file for uploading to overleaf (y/N) : ")
+        if self.zipPref == "y" or self.zipPref == "Y":
 
+            import zipfile
+            zipf = zipfile.ZipFile('output.zip', 'w', zipfile.ZIP_DEFLATED)
+            for root, dirs, files in os.walk("./outputfolder"):
+                for file in files:
+                    zipf.write(os.path.join(root, file))
+            zipf.close()
+            print("output.zip created sucessfully!")
 
     def verifyFiles(self):
         import os.path
@@ -27,8 +37,10 @@ class kickass:
             if not os.path.exists(sys.argv[i]):
                 print("Make sure those files exist")
                 exit()
-
-
+        if os.path.isdir("outputfolder"):
+            print("A directory called outputfolder already exists!")
+            print("please delete the directory to continue")
+            exit()
 
     def platformStuff(self):
         if platform == "win32":
@@ -39,7 +51,6 @@ class kickass:
             self.executable_name = self.basename
 
     def readfile(self):
-        print(sys.argv[self.currentIndex])
         try:
             with open(sys.argv[self.currentIndex], 'r') as file:
                 self.code = file.read()
@@ -57,6 +68,7 @@ class kickass:
         number_pattern = "--NUMBER--"
         name_pattern = "--NAME--"
         title_pattern = "--TITLE--"
+        aim_pattern = "--AIM--"
 
         if len(sys.argv) > 2 :
             if self.currentIndex == 1:
@@ -69,14 +81,20 @@ class kickass:
             with open('baseout.tex', 'r') as base:
                 self.text = base.read()
 
+        print()
+        aim = input("Enter the aim of this program : ")
+        print()
+
         self.text = re.sub(code_pattern,self.code, self.text)
         self.text = re.sub(algo_pattern,self.algorithm, self.text)
-        self.text = re.sub(screenshot_pattern,self.basename, self.text)
         self.text = re.sub(number_pattern,str(self.currentIndex), self.text)
         self.text = re.sub(name_pattern,self.name, self.text)
         self.text = re.sub(title_pattern,self.title, self.text)
+        self.text = re.sub(aim_pattern,aim, self.text)
         # print(self.text)
-        self.takescreenshot()
+        if self.screenshot_pref != "n" and self.screenshot_pref != "N":
+            self.text = re.sub(screenshot_pattern,self.basename, self.text)
+            self.takescreenshot()
 
     def kick(self):
         print("Welcome to the script ! ")
@@ -84,6 +102,17 @@ class kickass:
 
         self.name = input("Enter your name : ")
         self.title = input("Enter the title : ")
+
+        self.screenshot_pref = input("Do you want to take screenshot ? (Y/n) : ")
+        print(self.screenshot_pref)
+
+        try:
+            os.mkdir("outputfolder")
+            os.mkdir("outputfolder/pics")
+        except FileExistsError:
+            print("A directory called outputfolder already exists!")
+            print("please delete the directory to continue")
+            exit()
 
         for i in range(1, len(sys.argv)):
             self.currentIndex = i
@@ -131,19 +160,21 @@ class kickass:
                 print("You have some compile issues")
                 exit()
             else:
-                print("compilation done sucessfully")
+                print("compilation completed sucessfully")
         except :
             print("Make sure you have gcc installed and it is in your $PATH variable")
             exit()
         self.run()
 
     def run(self):
-        print("Compilation completed sucessfully!")
         print()
-        print("Running the program for making screeshot ")
         if platform == "win32":
+            print("Opening cmd for making screenshot")
+            print("run .\\"+ self.executable_name)
             run_cmd = subprocess.run(["cmd.exe"])
         elif platform == "linux":
+            print("Opening bash for making screenshot")
+            print("run ./"+ self.executable_name)
             run_bash = subprocess.run(["bash"])
 
     def takescreenshot(self):
@@ -171,7 +202,7 @@ class kickass:
         elif platform=="linux":
             try:
                 outputimage = self.basename + ".png"
-                maim_ret = subprocess.run(["maim", "-s",outputimage])
+                maim_ret = subprocess.run(["maim", "-s", "outputfolder/pics/"+ outputimage])
             except PermissionError:
                 print("Make sure maim is installed")
 
@@ -180,5 +211,3 @@ a = kickass()
 # a.verifyFiles()
 # a.readfile()
 # a.replace_stuff()
-
-
